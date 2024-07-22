ae376ac252e47f788837fca1210eb77fd45e0d03
0xfraan <franek@krawczyk.com>
2024-07-22 02:51:45 +0200

    site

---
+++ site/docs/components/FellowshipBadge.tsx
@@ -0,0 +1,19 @@
+
+export function FellowshipBadge() {
+  return (
+    <a href="https://www.paradigm.xyz/2024/06/paradigm-fellowship-2024" target="_blank" rel="noreferrer noopener">
+      <img
+        alt="Paradigm Fellowship Submission"
+        className="vocs_Logo_logoLight"
+        src="fellowship_black.png"
+        style={{ height: 80 }}
+      />
+      <img
+        alt="Paradigm Fellowship Submission"
+        className="vocs_Logo_logoDark"
+        src="fellowship_white.png"
+        style={{ height: 80 }}
+      />
+    </a>
+  )
+}
---
+++ site/vocs.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  title: 'gaussian',
+  iconUrl: '/icon.png',
+  theme: {
+    accentColor: '#60A8FA',
+  },
+  logoUrl: {
+    light: '/logo_black.png',
+    dark: '/logo_white.png',
+  },
+  socials: [
+    {
+      icon: 'discord',
+      link: 'https://discord.com/users/294445288377024513',
+    },
+    {
+      icon: 'github',
+      link: 'https://github.com/0xfraan',
+    },
+    {
+      icon: 'x',
+      link: 'https://x.com/0xfraan',
+    },
+    {
+      icon: 'warpcast',
+      link: 'https://warpcast.com/ufolud',
+    },
+  ],
+})
